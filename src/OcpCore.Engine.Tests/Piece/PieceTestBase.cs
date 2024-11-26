8e7b833ef570c38ca7602c1dab04e4d93ed72d7b
Stevo John <stevehuwjohn@gmail.com>
2024-11-26 14:40:52 +0000

    Fixing rook tests

--- src/OcpCore.Engine.Tests/Piece/PieceTestBase.cs
+++ src/OcpCore.Engine.Tests/Piece/PieceTestBase.cs
@@ -7,12 +7,17 @@ public class PieceTestBase
     protected static void AssertExpectedMoves(string expected, List<int> moves)
     {
         var expectedMoves = expected.Split(',').Select(int.Parse).ToArray();
-        
-        Assert.Equal(expectedMoves.Length, moves.Count);
 
         foreach (var move in expectedMoves)
         {
             Assert.Contains(move, moves);
         }
+
+        foreach (var move in moves)
+        {
+            Assert.Contains(move, expectedMoves);
+        }
+        
+        Assert.Equal(expectedMoves.Length, moves.Count);
     }
 }
--- src/OcpCore.Engine.Tests/Piece/RookTests.cs
+++ src/OcpCore.Engine.Tests/Piece/RookTests.cs
@@ -39,4 +39,22 @@ public class RookTests : PieceTestBase
         
         AssertExpectedMoves(expectedMoves, moves);
     }
+    
+    [Theory]
+    [InlineData("8/8/8/P7/8/8/8/8 b - - 0 1", 0, "1,2,3,4,5,6,7,8,16,24,32")]
+    [InlineData("8/8/8/8/8/8/8/3P4 b - - 0 1", 0, "1,2,3,8,16,24,32,40,48,56")]
+    [InlineData("8/8/8/1P5P/8/8/8/8 b - - 0 1", 35, "33,34,36,37,38,39,59,51,43,27,19,11,3")]
+    [InlineData("3P4/8/8/8/8/8/3P4/8 b - - 0 1", 35, "32,33,34,36,37,38,39,59,51,43,27,19,11")]
+    public void TakesPieceOfOpposingColour(string fen, int position, string expectedMoves)
+    {
+        var board = new Board(fen);
+
+        var rook = new Rook();
+
+        var moves = new List<int>();
+        
+        rook.GetMoves(board, position, Colour.Black, moves);
+        
+        AssertExpectedMoves(expectedMoves, moves);
+    }
 }
