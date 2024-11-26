ba2566ef1b5daa019a6dde5b1ec55cba20763d21
Stevo John <stevehuwjohn@gmail.com>
2024-11-26 18:22:37 +0000

    Updating Knight tests

--- src/OcpCore.Engine.Tests/Pieces/KnightTests.cs
+++ src/OcpCore.Engine.Tests/Pieces/KnightTests.cs
@@ -23,13 +23,15 @@ public class KnightTests : PieceTestBase<Knight>
     }
     
     [Theory]
-    [InlineData("8/8/8/8/8/1p6/8/8 b - - 0 1", 0, "10")]
-    [InlineData("8/8/8/8/8/8/2p5/8 b - - 0 1", 0, "17")]
-    [InlineData("8/8/1p6/8/8/4p3/8/8 b - - 0 1", 35, "25,45,29,50,52,18")]
-    [InlineData("8/8/5p2/8/5p2/8/8/8 b - - 0 1", 35, "41,25,50,52,18,20")]
+    [InlineData("8/8/8/8/8/1p6/8/n7 b - - 0 1", 0, "10")]
+    [InlineData("8/8/8/8/8/8/2p5/n7 b - - 0 1", 0, "17")]
+    [InlineData("8/8/1p6/3n4/8/4p3/8/8 b - - 0 1", 35, "25,45,29,50,52,18")]
+    [InlineData("8/8/5p2/3n4/5p2/8/8/8 b - - 0 1", 35, "41,25,50,52,18,20")]
     public void IsBlockedByPieceOfOwnColour(string fen, int position, string expectedMoves)
     {
         var board = new Board(fen);
+
+        AssertPieceIsWhereExpected(board, position, Colour.Black);
 
         var piece = Piece;
 
@@ -41,13 +43,15 @@ public class KnightTests : PieceTestBase<Knight>
     }
     
     [Theory]
-    [InlineData("8/8/8/8/8/1P6/8/8 b - - 0 1", 0, "17,10")]
-    [InlineData("8/8/8/8/8/8/2P5/8 b - - 0 1", 0, "17,10")]
-    [InlineData("8/8/1P6/8/8/4P3/8/8 b - - 0 1", 35, "41,25,45,29,50,52,18,20")]
-    [InlineData("8/8/5P2/8/5P2/8/8/8 b - - 0 1", 35, "41,25,45,29,50,52,18,20")]
+    [InlineData("8/8/8/8/8/1P6/8/n7 b - - 0 1", 0, "17,10")]
+    [InlineData("8/8/8/8/8/8/2P5/n7 b - - 0 1", 0, "17,10")]
+    [InlineData("8/8/1P6/3n4/8/4P3/8/8 b - - 0 1", 35, "41,25,45,29,50,52,18,20")]
+    [InlineData("8/8/5P2/3n4/5P2/8/8/8 b - - 0 1", 35, "41,25,45,29,50,52,18,20")]
     public void TakesPieceOfOpposingColour(string fen, int position, string expectedMoves)
     {
         var board = new Board(fen);
+
+        AssertPieceIsWhereExpected(board, position, Colour.Black);
 
         var piece = Piece;
 
@@ -59,11 +63,13 @@ public class KnightTests : PieceTestBase<Knight>
     }
 
     [Theory]
-    [InlineData("8/8/8/8/8/1P6/8/8 b - - 0 1", 0, true, 17)]
-    [InlineData("8/8/8/8/8/1p6/8/8 b - - 0 1", 0, false, 0)]
+    [InlineData("8/8/8/8/8/1P6/8/n7 b - - 0 1", 0, true, 17)]
+    [InlineData("8/8/8/8/8/1p6/8/n7 b - - 0 1", 0, false, 0)]
     public void ReportsCapturesCorrectly(string fen, int position, bool captureExpected, int captureCell)
     {
         var board = new Board(fen);
+
+        AssertPieceIsWhereExpected(board, position, Colour.Black);
 
         var piece = Piece;
 
