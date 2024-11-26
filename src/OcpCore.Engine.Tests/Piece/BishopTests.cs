793490513dc073833239b947c56f9e0620c2986d
Stevo John <stevehuwjohn@gmail.com>
2024-11-26 15:23:08 +0000

    Bishop tests

--- src/OcpCore.Engine.Tests/Piece/BishopTests.cs
+++ src/OcpCore.Engine.Tests/Piece/BishopTests.cs
@@ -25,6 +25,7 @@ public class BishopTests : PieceTestBase
     [Theory]
     [InlineData("8/8/8/8/3p4/8/8/8 b - - 0 1", 0, "9,18")]
     [InlineData("p7/8/8/8/8/1p6/8/8 b - - 0 1", 35, "42,49,26,44,53,62,28,21,14,7")]
+    [InlineData("8/1p6/8/8/8/8/8/7p", 35, "42,26,17,8,44,53,62,28,21,14")]
     public void IsBlockedByPieceOfOwnColour(string fen, int position, string expectedMoves)
     {
         var board = new Board(fen);
@@ -41,6 +42,7 @@ public class BishopTests : PieceTestBase
     [Theory]
     [InlineData("8/8/8/8/3P4/8/8/8 b - - 0 1", 0, "9,18,27")]
     [InlineData("P7/8/8/8/8/1P6/8/8 b - - 0 1", 35, "42,49,56,26,17,44,53,62,28,21,14,7")]
+    [InlineData("8/1P6/8/8/8/8/8/7P", 35, "42,49,26,17,8,44,53,62,28,21,14,7")]
     public void TakesPieceOfOpposingColour(string fen, int position, string expectedMoves)
     {
         var board = new Board(fen);
