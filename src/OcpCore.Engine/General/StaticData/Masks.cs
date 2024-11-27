e5c3be3207ec83a97c60a0e58bc549d1528e5eaf
Stevo John <stevehuwjohn@gmail.com>
2024-11-27 11:30:47 +0000

    Removed unused mask

--- src/OcpCore.Engine/General/StaticData/Masks.cs
+++ src/OcpCore.Engine/General/StaticData/Masks.cs
@@ -3,7 +3,6 @@ namespace OcpCore.Engine.General.StaticData;
 public static class Masks
 {
     public const byte File = 0b0000_0111;
-    public const byte Kind = 0b0000_0111;
     public const byte Colour = 0b0000_1000;
     public const int CastleStatus = 0b0000_1111;
     public const int EnPassantTarget = 0b0000_1111_1110_0000;
