acbafd900e442a71087351bce3537c0e0c9d894b
Stevo John <stevehuwjohn@gmail.com>
2024-11-27 12:52:34 +0000

    Starting work on Core

--- src/OcpCore.Engine/Core.cs
+++ src/OcpCore.Engine/Core.cs
@@ -1,3 +1,7 @@
+using OcpCore.Engine.Extensions;
+using OcpCore.Engine.General;
+using OcpCore.Engine.General.StaticData;
+
 namespace OcpCore.Engine;
 
 public class Core
@@ -5,4 +9,42 @@ public class Core
     public const string Name = "Ocp Core Chess";
 
     public const string Author = "Stevo John";
+
+    private const int DefaultDepth = 6;
+
+    private readonly Board _board;
+
+    private readonly int _defaultDepth;
+    
+    public Core(int defaultDepth = DefaultDepth)
+    {
+        _board = new Board(Constants.InitialBoardFen);
+
+        _defaultDepth = defaultDepth;
+    }
+
+    public Core(string fen, int defaultDepth = DefaultDepth)
+    {
+        _board = new Board(fen);
+
+        _defaultDepth = defaultDepth;
+    }
+
+    public void MakeMove(string move)
+    {
+        var position = move[..2].FromStandardNotation();
+
+        var target = move[2..].FromStandardNotation();
+        
+        _board.MakeMove(position, target);
+    }
+
+    public void GetMove()
+    {
+        GetMove(_defaultDepth);
+    }
+
+    public void GetMove(int depth)
+    {
+    }
 }
--- src/OcpCore.Engine/General/StaticData/Constants.cs
+++ src/OcpCore.Engine/General/StaticData/Constants.cs
@@ -18,4 +18,6 @@ public static class Constants
     public static readonly (int RankDelta, int FileDelta)[] DiagonalMoves = [(-1, -1), (-1, 1), (1, -1), (1, 1)];
     public static readonly (int RankDelta, int FileDelta)[] DirectionalMoves = [(-1, 0), (0, -1), (1, 0), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)];
     public static readonly (int RankDelta, int FileDelta)[] KnightMoves = [(2, -1), (2, 1), (-2, -1), (-2, 1), (1, -2), (-1, -2), (1, 2), (-1, 2)];
+
+    public const string InitialBoardFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 }
