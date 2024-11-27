0a8e1d9d7a0d329c999aa36be300dbeb443d98ba
Stevo John <stevehuwjohn@gmail.com>
2024-11-27 12:32:16 +0000

    Fixing up tests

--- src/OcpCore.Engine.Tests/General/StateTests.cs
+++ src/OcpCore.Engine.Tests/General/StateTests.cs
@@ -1,4 +1,5 @@
 using OcpCore.Engine.General;
+using OcpCore.Engine.General.StaticData;
 using OcpCore.Engine.Pieces;
 using Xunit;
 
@@ -51,7 +52,11 @@ public class StateTests
         Assert.Equal(enPassantTarget, state.EnPassantTarget);
         
         Assert.Equal(castleStatus, state.CastleStatus);
+                
+        Assert.Equal(whiteScore, state.WhiteScore);
         
+        Assert.Equal(blackScore, state.BlackScore);
+
         state.RemoveCastleRights(rightToRemove);
         
         Assert.Equal(player, state.Player);
@@ -85,7 +90,11 @@ public class StateTests
         Assert.Equal(enPassantTarget, state.EnPassantTarget);
         
         Assert.Equal(castleStatus, state.CastleStatus);
+                
+        Assert.Equal(whiteScore, state.WhiteScore);
         
+        Assert.Equal(blackScore, state.BlackScore);
+
         state.SetEnPassantTarget(newEnPassantTarget);
         
         Assert.Equal(player, state.Player);
@@ -100,29 +109,33 @@ public class StateTests
     }   
     
     [Theory]
-    [InlineData(Colour.White, Castle.WhiteQueenSide | Castle.BlackKingSide, 49, 12, 34, 24, 68)]
-    [InlineData(Colour.Black, Castle.WhiteQueenSide | Castle.BlackKingSide, 42, 56, 78, 12, 34)]
-    [InlineData(Colour.White, Castle.NotAvailable, 18, 55, 12, 34, 56)]
-    [InlineData(Colour.Black, Castle.NotAvailable, 34, 43, 12, 34, 12)]
-    [InlineData(Colour.White, Castle.WhiteQueenSide, 30, 50, 12, 34, 56)]
-    [InlineData(Colour.Black, Castle.BlackKingSide, 50, 34, 12, 34, 0)]
-    [InlineData(Colour.White, Castle.WhiteQueenSide | Castle.WhiteKingSide | Castle.BlackQueenSide | Castle.BlackKingSide, 33, 54, 12, 34, 0)]
-    [InlineData(Colour.Black, Castle.All, 29, 63, 12, 34, 76)]
-    [InlineData(Colour.White, Castle.NotAvailable, null, 12, 0, 0, 87)]
-    [InlineData(Colour.Black, Castle.NotAvailable, null, 14, 0, 0, 98)]
-    public void SetScoresOperatesCorrectly(Colour player, Castle castleStatus, int? enPassantTarget, int whiteScore, int blackScore, int newWhiteScore, int newBlackScore)
+    [InlineData(Colour.White, Castle.WhiteQueenSide | Castle.BlackKingSide, 49, -Scores.Knight, Scores.Pawn)]
+    [InlineData(Colour.Black, Castle.WhiteQueenSide | Castle.BlackKingSide, 42, 0, Scores.Queen)]
+    [InlineData(Colour.White, Castle.NotAvailable, 18, -Scores.Pawn, Scores.Pawn)]
+    [InlineData(Colour.Black, Castle.NotAvailable, 34, -Scores.Knight, Scores.Bishop)]
+    [InlineData(Colour.White, Castle.WhiteQueenSide, 30, -Scores.Rook, Scores.Pawn)]
+    [InlineData(Colour.Black, Castle.BlackKingSide, 50, -Scores.Knight, 0)]
+    [InlineData(Colour.White, Castle.WhiteQueenSide | Castle.WhiteKingSide | Castle.BlackQueenSide | Castle.BlackKingSide, 33, -Scores.Queen, Scores.Pawn)]
+    [InlineData(Colour.Black, Castle.All, 29, -Scores.Knight, Scores.Pawn)]
+    [InlineData(Colour.White, Castle.NotAvailable, null, -Scores.Knight, Scores.Pawn)]
+    [InlineData(Colour.Black, Castle.NotAvailable, null, 0, Scores.Pawn)]
+    public void SetScoresOperatesCorrectly(Colour player, Castle castleStatus, int? enPassantTarget, int whiteDelta, int blackDelta)
     {
-        var state = new State(player, castleStatus, enPassantTarget, whiteScore, blackScore);
+        var state = new State(player, castleStatus, enPassantTarget, Scores.Initial, Scores.Initial);
         
         Assert.Equal(player, state.Player);
         
         Assert.Equal(enPassantTarget, state.EnPassantTarget);
         
         Assert.Equal(castleStatus, state.CastleStatus);
+                
+        Assert.Equal(Scores.Initial, state.WhiteScore);
         
-        state.UpdateWhiteScore(newWhiteScore);
+        Assert.Equal(Scores.Initial, state.BlackScore);
 
-        state.UpdateBlackScore(newBlackScore);
+        state.UpdateWhiteScore(whiteDelta);
+
+        state.UpdateBlackScore(blackDelta);
         
         Assert.Equal(player, state.Player);
         
@@ -130,8 +143,8 @@ public class StateTests
         
         Assert.Equal(castleStatus, state.CastleStatus);
         
-        Assert.Equal(newWhiteScore, state.WhiteScore);
+        Assert.Equal(Scores.Initial + whiteDelta, state.WhiteScore);
         
-        Assert.Equal(newBlackScore, state.BlackScore);
+        Assert.Equal(Scores.Initial + blackDelta, state.BlackScore);
     }
 }
