a5383583681a80e89590166766638db7ca0cfa8b
Stevo John <stevehuwjohn@gmail.com>
2024-11-28 14:05:34 +0000

    Added CoreTests class

--- src/OcpCore.Engine.Tests/CoreTests.cs
+++ src/OcpCore.Engine.Tests/CoreTests.cs
@@ -5,18 +5,18 @@ namespace OcpCore.Engine.Tests;
 
 public class CoreTests
 {
-    // [Theory]
+    [Theory]
     // [InlineData(1, 20)]
     // [InlineData(2, 400)]
     // [InlineData(3, 8_902)]
-    // [InlineData(4, 8_902)]
-    // [InlineData(5, 8_902)]
-    // public void ReturnsExpectedCountAtPly(int ply, int expectedCount)
-    // {
-    //     var core = new Core(Colour.White);
-    //
-    //     core.GetMove(ply);
-    //     
-    //     Assert.Equal(expectedCount, core.GetDepthCount(ply));
-    // }
+    // [InlineData(4, 197_281)]
+    [InlineData(5, 4_865_609)]
+    public void ReturnsExpectedCountAtPly(int ply, int expectedCount)
+    {
+        var core = new Core(Colour.White);
+    
+        core.GetMove(ply);
+        
+        Assert.Equal(expectedCount, core.GetDepthCount(ply));
+    }
 }
