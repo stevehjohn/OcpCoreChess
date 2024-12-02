b7453ec7526a12cc1777a09b2262866189bcba6e
Stevo John <stevehuwjohn@gmail.com>
2024-12-02 21:20:47 +0000

    H/V B/O works I think

--- src/OcpCore.Engine/Pieces/Piece.cs
+++ src/OcpCore.Engine/Pieces/Piece.cs
@@ -1,5 +1,7 @@
+using System.Numerics;
 using OcpCore.Engine.Bitboards;
 using OcpCore.Engine.Extensions;
+using Plane = OcpCore.Engine.Bitboards.Plane;
 
 namespace OcpCore.Engine.Pieces;
 
@@ -26,4 +28,66 @@ public abstract class Piece
     }
 
     protected abstract ulong GetMoves(Game game, Plane colour, Plane opponentColour, int position);
+
+    protected ulong GetHorizontalSlidingMoves(Game game, Plane colour, Plane opponentColour, int position)
+    {
+        var positionBit = 1ul << position;
+
+        var mask = Moves[Kind.Rook][MoveSet.Horizontal][position];
+        
+        var rightBlockers = (game[colour] | game[opponentColour])& mask & (~(positionBit - 1) - positionBit);
+
+        var firstRightBlocker = rightBlockers != 0 ? BitOperations.TrailingZeroCount(rightBlockers) : 64;
+
+        var rightMask = firstRightBlocker < 64 ? (1ul << firstRightBlocker) - 1 - positionBit : ulong.MaxValue;
+
+        if (rightBlockers != 0 && (game[opponentColour] & (1ul << firstRightBlocker)) != 0)
+        {
+            rightMask |= 1ul << firstRightBlocker;
+        }
+
+        var leftBlockers = game[colour] & mask & positionBit - 1;
+        
+        var firstLeftBlocker = leftBlockers != 0 ? BitOperations.LeadingZeroCount(leftBlockers) - 1 : 64;
+        
+        var leftMask = firstLeftBlocker < 64 ? ~((1ul << (63 - firstLeftBlocker)) - 1 ): ulong.MaxValue;
+
+        if (leftBlockers != 0 && (game[opponentColour] & (1ul << firstLeftBlocker)) != 0)
+        {
+            leftMask |= 1ul << firstRightBlocker;
+        }
+
+        return leftMask & rightMask & mask;
+    }
+
+    protected ulong GetVerticalSlidingMoves(Game game, Plane colour, Plane opponentColour, int position)
+    {
+        var positionBit = 1ul << position;
+
+        var mask = Moves[Kind.Rook][MoveSet.Vertical][position];
+
+        var upBlockers = game[colour] & mask & (~(positionBit - 1) - positionBit);
+
+        var firstUpBlocker = upBlockers != 0 ? BitOperations.TrailingZeroCount(upBlockers) : 64;
+
+        var upMask = firstUpBlocker < 64 ? (1ul << firstUpBlocker) - 1 - positionBit : ulong.MaxValue;
+        
+        if (upBlockers != 0 && (game[opponentColour] & (1ul << firstUpBlocker)) != 0)
+        {
+            upMask |= 1ul << firstUpBlocker;
+        }
+
+        var downBlockers = game[colour] & mask & positionBit - 1;
+        
+        var firstDownBlocker = downBlockers != 0 ? BitOperations.LeadingZeroCount(downBlockers) - 1 : 64;
+        
+        var downMask = firstDownBlocker < 64 ? ~((1ul << (63 - firstDownBlocker)) - 1 ): ulong.MaxValue;
+        
+        if (downBlockers != 0 && (game[opponentColour] & (1ul << firstDownBlocker)) != 0)
+        {
+            downMask |= 1ul << firstDownBlocker;
+        }
+
+        return upMask & downMask & mask;
+    }
 }
--- src/OcpCore.Engine/Pieces/Rook.cs
+++ src/OcpCore.Engine/Pieces/Rook.cs
@@ -1,4 +1,3 @@
-using System.Numerics;
 using OcpCore.Engine.Bitboards;
 using Plane = OcpCore.Engine.Bitboards.Plane;
 
@@ -21,67 +20,5 @@ public class Rook : Piece
         moves |= GetVerticalSlidingMoves(game, colour, opponentColour, position);
         
         return moves;
-    }
-
-    private ulong GetHorizontalSlidingMoves(Game game, Plane colour, Plane opponentColour, int position)
-    {
-        var positionBit = 1ul << position;
-
-        var mask = Moves[Kind.Rook][MoveSet.Horizontal][position];
-        
-        var rightBlockers = (game[colour] | game[opponentColour])& mask & (~(positionBit - 1) - positionBit);
-
-        var firstRightBlocker = rightBlockers != 0 ? BitOperations.TrailingZeroCount(rightBlockers) : 64;
-
-        var rightMask = firstRightBlocker < 64 ? (1ul << firstRightBlocker) - 1 - positionBit : ulong.MaxValue;
-
-        if (rightBlockers != 0 && (game[opponentColour] & (1ul << firstRightBlocker)) != 0)
-        {
-            rightMask |= 1ul << firstRightBlocker;
-        }
-
-        var leftBlockers = game[colour] & mask & positionBit - 1;
-        
-        var firstLeftBlocker = leftBlockers != 0 ? BitOperations.LeadingZeroCount(leftBlockers) - 1 : 64;
-        
-        var leftMask = firstLeftBlocker < 64 ? ~((1ul << (63 - firstLeftBlocker)) - 1 ): ulong.MaxValue;
-
-        if (leftBlockers != 0 && (game[opponentColour] & (1ul << firstLeftBlocker)) != 0)
-        {
-            leftMask |= 1ul << firstRightBlocker;
-        }
-
-        return leftMask & rightMask & mask;
-    }
-
-    private ulong GetVerticalSlidingMoves(Game game, Plane colour, Plane opponentColour, int position)
-    {
-        var positionBit = 1ul << position;
-
-        var mask = Moves[Kind.Rook][MoveSet.Vertical][position];
-
-        var upBlockers = game[colour] & mask & (~(positionBit - 1) - positionBit);
-
-        var firstUpBlocker = upBlockers != 0 ? BitOperations.TrailingZeroCount(upBlockers) : 64;
-
-        var upMask = firstUpBlocker < 64 ? (1ul << firstUpBlocker) - 1 - positionBit : ulong.MaxValue;
-        
-        if (upBlockers != 0 && (game[opponentColour] & (1ul << firstUpBlocker)) != 0)
-        {
-            upMask |= 1ul << firstUpBlocker;
-        }
-
-        var downBlockers = game[colour] & mask & positionBit - 1;
-        
-        var firstDownBlocker = downBlockers != 0 ? BitOperations.LeadingZeroCount(downBlockers) - 1 : 64;
-        
-        var downMask = firstDownBlocker < 64 ? ~((1ul << (63 - firstDownBlocker)) - 1 ): ulong.MaxValue;
-        
-        if (downBlockers != 0 && (game[opponentColour] & (1ul << firstDownBlocker)) != 0)
-        {
-            downMask |= 1ul << firstDownBlocker;
-        }
-
-        return upMask & downMask & mask;
     }
 }
