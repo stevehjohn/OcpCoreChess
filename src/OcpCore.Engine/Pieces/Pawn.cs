b05b65e24a8b00427394d935cc105ff07ed0480c
Stevo John <stevehuwjohn@gmail.com>
2024-12-03 15:20:12 +0000

    Better?

--- src/OcpCore.Engine/Pieces/Pawn.cs
+++ src/OcpCore.Engine/Pieces/Pawn.cs
@@ -1,4 +1,6 @@
 using OcpCore.Engine.Bitboards;
+using OcpCore.Engine.General;
+using OcpCore.Engine.General.StaticData;
 
 namespace OcpCore.Engine.Pieces;
 
@@ -17,6 +19,23 @@ public class Pawn : Piece
         var moveSet = colour == Plane.White ? MoveSet.PawnToBlack : MoveSet.PawnToWhite;
 
         var moves = Moves[Kind.Pawn][moveSet][position] & ~game[opponentColour] & ~game[colour];
+
+        var rank = Cell.GetRank(position);
+
+        // TODO: Magic numbers
+        switch (rank)
+        {
+            case Ranks.WhitePawnRank:
+                moves &= ~((game[colour] & (Masks.ByteMask << 16)) << 8);
+
+                moves &= ~((game[opponentColour] & (Masks.ByteMask << 16)) << 8);
+                break;
+            case Ranks.BlackPawnRank:
+                moves &= ~((game[colour] & (Masks.ByteMask << 40)) >> 8);
+
+                moves &= ~((game[opponentColour] & (Masks.ByteMask << 40)) >> 8);
+                break;
+        }
 
         var attackSet = colour == Plane.White ? MoveSet.PawnWhiteAttack : MoveSet.PawnBlackAttack;
 
