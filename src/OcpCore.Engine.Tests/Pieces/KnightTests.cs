fb3bb32139e19924f254443e604341d4f8d86ba5
Stevo John <stevehuwjohn@gmail.com>
2024-12-03 12:26:14 +0000

    Knight

--- src/OcpCore.Engine.Tests/Pieces/KnightTests.cs
+++ src/OcpCore.Engine.Tests/Pieces/KnightTests.cs
@@ -29,8 +29,8 @@ public class KnightTests
     [Theory]
     [InlineData("8/8/8/8/8/8/2P5/N7 w - - 0 1", 0,
         0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0010_0000_0000_0000_0000)]
-    // [InlineData("8/8/8/3n4/8/8/8/8 b - - 0 1", 35,
-    //     0b0000_0000_0001_0100_0010_0010_0000_0000_0010_0010_0001_0100_0000_0000_0000_0000)]
+    [InlineData("8/8/1P6/3N4/8/8/8/8 w - - 0 1", 35,
+        0b0000_0000_0001_0100_0010_0000_0000_0000_0010_0010_0001_0100_0000_0000_0000_0000)]
     public void IsBlockedByPieceOfOwnColour(string fen, int position, ulong expectedMoves)
     {
         var game = new Game();
@@ -47,8 +47,8 @@ public class KnightTests
     [Theory]
     [InlineData("8/8/8/8/8/8/2p5/N7 w - - 0 1", 0,
         0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0010_0000_0100_0000_0000)]
-    // [InlineData("8/8/8/3n4/8/8/8/8 b - - 0 1", 35,
-    //     0b0000_0000_0001_0100_0010_0010_0000_0000_0010_0010_0001_0100_0000_0000_0000_0000)]
+    [InlineData("8/8/1p6/3N4/8/8/8/8 w - - 0 1", 35,
+        0b0000_0000_0001_0100_0010_0010_0000_0000_0010_0010_0001_0100_0000_0000_0000_0000)]
     public void TakesPieceOfOpposingColour(string fen, int position, ulong expectedMoves)
     {
         var game = new Game();
