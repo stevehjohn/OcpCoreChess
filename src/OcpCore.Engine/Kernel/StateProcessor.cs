32d917f6dd92fb4dee170fac3b3f0a6118976f25
Stevo John <stevehuwjohn@gmail.com>
2024-12-07 19:37:03 +0000

    Work in progress

--- src/OcpCore.Engine/Kernel/StateProcessor.cs
+++ src/OcpCore.Engine/Kernel/StateProcessor.cs
@@ -5,9 +5,13 @@ namespace OcpCore.Engine.Kernel;
 
 public class StateProcessor
 {
+    private static readonly int Threads = Environment.ProcessorCount - 2;
+    
     private readonly int _maxDepth;
 
     private readonly PriorityQueue<(Game game, int depth), int> _centralQueue;
+
+    private readonly CancellationToken _cancellationToken;
     
     private readonly PriorityQueue<(Game game, int depth), int> _localQueue = new();
 
@@ -15,11 +19,13 @@ public class StateProcessor
     
     private readonly long[][] _outcomes;
 
-    public StateProcessor(int maxDepth, PriorityQueue<(Game game, int depth), int> centralQueue)
+    public StateProcessor(int maxDepth, PriorityQueue<(Game game, int depth), int> centralQueue, CancellationToken cancellationToken)
     {
         _maxDepth = maxDepth;
 
         _centralQueue = centralQueue;
+
+        _cancellationToken = cancellationToken;
         
         _depthCounts = new long[maxDepth + 1];
 
@@ -31,5 +37,42 @@ public class StateProcessor
 
             _outcomes[i] = new long[Constants.MoveOutcomes + 1];
         }
+    }
+
+    public void StartProcessing()
+    {
+        while (_centralQueue.Count > 0)
+        {
+            lock (_centralQueue)
+            {
+                for (var i = 0; i < Math.Max(1, _centralQueue.Count / Threads); i++)
+                {
+                    if (_centralQueue.TryDequeue(out var workItem, out var priority))
+                    {
+                        _localQueue.Enqueue(workItem, priority);
+                        
+                        continue;
+                    }
+                    
+                    break;
+                }
+            }
+
+            while (_localQueue.Count > 0)
+            {
+                if (_cancellationToken.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                var (game, depth) = _localQueue.Dequeue();
+                
+                ProcessWorkItem(game, depth);
+            }
+        }
+    }
+
+    private void ProcessWorkItem(Game game, int depth)
+    {
     }
 }
