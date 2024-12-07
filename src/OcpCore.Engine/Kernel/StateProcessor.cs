b5db91fa1d50138d4107c48da9410a3b63281474
Stevo John <stevehuwjohn@gmail.com>
2024-12-07 19:46:47 +0000

    Refactoring

--- src/OcpCore.Engine/Kernel/StateProcessor.cs
+++ src/OcpCore.Engine/Kernel/StateProcessor.cs
@@ -1,5 +1,9 @@
+using System.Numerics;
 using OcpCore.Engine.Bitboards;
+using OcpCore.Engine.Extensions;
+using OcpCore.Engine.General;
 using OcpCore.Engine.General.StaticData;
+using Plane = OcpCore.Engine.Bitboards.Plane;
 
 namespace OcpCore.Engine.Kernel;
 
@@ -10,20 +14,24 @@ public class StateProcessor
     private readonly int _maxDepth;
 
     private readonly PriorityQueue<(Game game, int depth), int> _centralQueue;
-
-    private readonly CancellationToken _cancellationToken;
     
     private readonly PriorityQueue<(Game game, int depth), int> _localQueue = new();
 
+    private readonly CancellationToken _cancellationToken;
+
+    private readonly long[] _centralDepthCounts;
+    
     private readonly long[] _depthCounts;
     
     private readonly long[][] _outcomes;
 
-    public StateProcessor(int maxDepth, PriorityQueue<(Game game, int depth), int> centralQueue, CancellationToken cancellationToken)
+    public StateProcessor(int maxDepth, PriorityQueue<(Game game, int depth), int> centralQueue, long[] centralDepthCounts, CancellationToken cancellationToken)
     {
         _maxDepth = maxDepth;
 
         _centralQueue = centralQueue;
+
+        _centralDepthCounts = centralDepthCounts;
 
         _cancellationToken = cancellationToken;
         
@@ -74,5 +82,148 @@ public class StateProcessor
 
     private void ProcessWorkItem(Game game, int depth)
     {
+        var player = game.State.Player;
+
+        var ply = _maxDepth - depth + 1;
+
+        var pieces = game[(Plane) player];
+
+        var cell = PopPiecePosition(ref pieces);
+
+        while (cell > -1)
+        {
+            var kind = game.GetKind(cell);
+
+            var moves = PieceCache.Get(kind).GetMoves(game, cell);
+
+            var move = Piece.PopNextMove(ref moves);
+
+            while (move > -1)
+            {
+                var copy = new Game(game);
+
+                var outcomes = copy.MakeMove(cell, move);
+
+                if (copy.IsKingInCheck((Plane) player))
+                {
+                    move = Piece.PopNextMove(ref moves);
+
+                    continue;
+                }
+
+                IncrementCounts(ply);
+
+                if (copy.IsKingInCheck((Plane) player.Invert()))
+                {
+                    outcomes |= MoveOutcome.Check;
+
+                    if (! CanMove(copy, player.Invert()))
+                    {
+                        outcomes |= MoveOutcome.CheckMate;
+                    }
+                }
+
+                IncrementOutcomes(ply, outcomes);
+
+                if (depth > 1 && (outcomes & MoveOutcome.CheckMate) == 0)
+                {
+                    Enqueue(copy, depth - 1, MoveOutcome.CheckMate - outcomes);
+                }
+
+                move = Piece.PopNextMove(ref moves);
+            }
+
+            cell = PopPiecePosition(ref pieces);
+        }
+    }
+    
+    private static int PopPiecePosition(ref ulong pieces)
+    {
+        var emptyMoves = BitOperations.TrailingZeroCount(pieces);
+
+        if (emptyMoves == 64)
+        {
+            return -1;
+        }
+
+        pieces ^= 1ul << emptyMoves;
+
+        return emptyMoves;
+    }
+
+    private static bool CanMove(Game game, Colour colour)
+    {
+        var pieces = game[(Plane) colour];
+
+        var cell = PopPiecePosition(ref pieces);
+
+        while (cell > -1)
+        {
+            var kind = game.GetKind(cell);
+
+            var moves = PieceCache.Get(kind).GetMoves(game, cell);
+
+            var move = Piece.PopNextMove(ref moves);
+
+            while (move > -1)
+            {
+                var copy = new Game(game);
+
+                copy.MakeMove(cell, move);
+
+                if (copy.IsKingInCheck((Plane) colour))
+                {
+                    move = Piece.PopNextMove(ref moves);
+                
+                    continue;
+                }
+
+                return true;
+            }
+
+            cell = PopPiecePosition(ref pieces);
+        }
+
+        return false;
+    }
+    
+    private void IncrementCounts(int ply)
+    {
+        _depthCounts[ply]++;
+
+        if (_depthCounts[ply] > 1_000)
+        {
+            Interlocked.Add(ref _centralDepthCounts[ply], _depthCounts[ply]);
+        
+            _depthCounts[ply] = 0;
+        }
+    }
+
+    private void IncrementOutcomes(int ply, MoveOutcome outcomes)
+    {
+        while (outcomes > 0)
+        {
+            var outcome = BitOperations.TrailingZeroCount((int) outcomes);
+
+            _outcomes[ply][outcome + 1]++;
+
+            outcomes ^= (MoveOutcome) (1 << outcome);
+        }
+    }
+
+    private void Enqueue(Game game, int depth, int priority)
+    {
+        // ReSharper disable once InconsistentlySynchronizedField - Doesn't need to be exactly 1,000.
+        if (_centralQueue.Count < 1_000)
+        {
+            lock (_centralQueue)
+            {
+                _centralQueue.Enqueue((game, depth), priority);
+            }
+        }
+        else
+        {
+            _localQueue.Enqueue((game, depth), priority);
+        }
     }
 }
