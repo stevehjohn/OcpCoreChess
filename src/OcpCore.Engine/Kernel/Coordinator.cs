2503db133b70ba4383822fdbe4a092cda5e7c7ed
Stevo John <stevehuwjohn@gmail.com>
2024-12-08 12:53:02 +0000

    Wowzer

--- src/OcpCore.Engine/Kernel/Coordinator.cs
+++ src/OcpCore.Engine/Kernel/Coordinator.cs
@@ -105,7 +105,7 @@ public sealed class Coordinator : IDisposable
         {
             for (var depth = 1; depth <= _maxDepth; depth++)
             {
-                for (var outcome = 0; outcome < Constants.MoveOutcomes; outcome++)
+                for (var outcome = 0; outcome <= Constants.MoveOutcomes; outcome++)
                 {
                     Interlocked.Add(ref _outcomes[depth][outcome], processor.GetOutcomeCount(depth, (MoveOutcome) (1 << outcome)));
                 }
