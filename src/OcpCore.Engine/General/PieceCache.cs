bebd1c56826f8137c3e2c68b1a779175bd9bcbf9
Stevo John <stevehuwjohn@gmail.com>
2024-12-11 14:23:07 +0000

    Tweak

--- src/OcpCore.Engine/General/PieceCache.cs
+++ src/OcpCore.Engine/General/PieceCache.cs
@@ -8,23 +8,18 @@ public class PieceCache
     
     public static PieceCache Instance => Instantiator.Value;
 
-    private readonly Piece[] _pieces;
+    private readonly Pieces _pieces;
 
     public Piece this[Kind kind] => _pieces[(int) kind];
     
     private PieceCache()
     {
-        _pieces =
-        [
-            null,
-            null,
-            new Pawn(),
-            new Rook(),
-            new Knight(),
-            new Bishop(),
-            new Queen(),
-            new King()
-        ];
+        _pieces[(int) Kind.Pawn] = new Pawn();
+        _pieces[(int) Kind.Rook] = new Rook();
+        _pieces[(int) Kind.Knight] = new Knight();
+        _pieces[(int) Kind.Bishop] = new Bishop();
+        _pieces[(int) Kind.Queen] = new Queen();
+        _pieces[(int) Kind.King] = new King();
     }
 
     private static PieceCache Instantiate()
---
+++ src/OcpCore.Engine/General/Pieces.cs
@@ -0,0 +1,10 @@
+using System.Runtime.CompilerServices;
+using OcpCore.Engine.General.StaticData;
+
+namespace OcpCore.Engine.General;
+
+[InlineArray(Constants.Planes)]
+public struct Pieces
+{
+    private Piece _piece;
+}
