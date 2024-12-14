5d15e2c45169fe4b62e06fd8ad297716bf2e7da2
Stevo John <stevehuwjohn@gmail.com>
2024-12-14 20:22:54 +0000

    Fixing UTs

--- src/OcpCore.Engine.Tests/Kernel/StateProcessorTests.cs
+++ src/OcpCore.Engine.Tests/Kernel/StateProcessorTests.cs
@@ -1,4 +1,5 @@
 using OcpCore.Engine.Bitboards;
+using OcpCore.Engine.General.StaticData;
 using OcpCore.Engine.Kernel;
 using Xunit;
 
@@ -9,23 +10,52 @@ public class StateProcessorTests
     [Fact]
     public void CancelsWhenRequested()
     {
+        var game = new Game();
+        
+        game.ParseFen(Constants.InitialBoardFen);
+        
         var queue = new PriorityQueue<(Game game, int depth, int root), int>();
+
+        queue.Enqueue((game, 1, -1), 0);
         
         var processor = new StateProcessor(queue);
-
-        for (var i = 0; i < 1_000_000; i++)
-        {
-            queue.Enqueue((new Game(), 1, -1), 0);
-        }
-
+        
         var cancellationTokenSource = new CancellationTokenSource();
 
         var cancellationToken = cancellationTokenSource.Token;
         
-        Task.Run(() => processor.StartProcessing(10, null, cancellationToken), cancellationToken);
+        cancellationTokenSource.Cancel();
+        
+        processor.StartProcessing(1, (_, _) => { }, cancellationToken);
         
         Thread.Sleep(100);
         
-        cancellationTokenSource.Cancel();
+        Assert.Equal(0, processor.GetDepthCount(1));
+    }
+
+    [Theory]
+    [InlineData("8/8/2P5/8/8/8/8/8 w - - 0 1", 1)]
+    [InlineData("8/2P5/8/8/8/8/8/8 w - - 0 1", 4)]
+    public void HandlesPromotionsCorrectly(string fen, int expectedTotal)
+    {
+        var game = new Game();
+        
+        game.ParseFen(fen);
+        
+        var queue = new PriorityQueue<(Game game, int depth, int root), int>();
+
+        queue.Enqueue((game, 1, -1), 0);
+        
+        var processor = new StateProcessor(queue);
+        
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        var cancellationToken = cancellationTokenSource.Token;
+        
+        processor.StartProcessing(1, (_, _) => { }, cancellationToken);
+        
+        Thread.Sleep(100);
+        
+        Assert.Equal(expectedTotal, processor.GetDepthCount(1));
     }
 }
